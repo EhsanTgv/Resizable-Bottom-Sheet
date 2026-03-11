32557413b48b0a59335eb7322d137ae67eb8be25
EhsanTgv <EhsanTgv@gmail.com>
2026-03-11 14:36:50 +0300

    Initiate views

--- Resizable Bottom Sheet/ContentView.swift
+++ Resizable Bottom Sheet/ContentView.swift
@@ -6,19 +6,71 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var selecedTravelName = "car.fill"
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: .bottomLeading){
+            Map(coordinateRegion: $region).ignoresSafeArea()
+            
+            Button(""){
+                
+            }
+            .buttonStyle(TravelModeButton(systemImageName: selecedTravelName))
+            .padding(30)
         }
-        .padding()
+    }
+    
+    var travelOptionView: some View {
+        VStack(spacing: 20){
+            Text("Select Travel Mode")
+            
+            HStack(spacing: 16){
+                Button(""){
+                    
+                }.buttonStyle(TravelModeButton(systemImageName: "airplane"))
+            }
+            
+            HStack(spacing: 16){
+                Button(""){
+                    
+                }.buttonStyle(TravelModeButton(systemImageName: "car.fill"))
+            }
+            
+            HStack(spacing: 16){
+                Button(""){
+                    
+                }.buttonStyle(TravelModeButton(systemImageName: "ferry.fill"))
+            }
+            
+            HStack(spacing: 16){
+                Button(""){
+                    
+                }.buttonStyle(TravelModeButton(systemImageName: "tram.fill"))
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct TravelModeButton: ButtonStyle {
+    let systemImageName: String
+    
+    func makeBody(configuration: Configuration) -> some View {
+        Image(systemName: systemImageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.white)
+            .frame(width: 33, height: 33)
+            .padding()
+            .background(Color.pink)
+            .clipShape(Circle())
+    }
 }
