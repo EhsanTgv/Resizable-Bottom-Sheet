d0a394b8a49a5c75ba70b1b8d7cbb1f91e97be31
EhsanTgv <EhsanTgv@gmail.com>
2026-03-11 14:14:33 +0300

    Initial Commit

---
+++ Resizable Bottom Sheet/Resizable_Bottom_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resizable_Bottom_SheetApp.swift
+//  Resizable Bottom Sheet
+//
+//  Created by Ehsan on 11.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Resizable_Bottom_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
